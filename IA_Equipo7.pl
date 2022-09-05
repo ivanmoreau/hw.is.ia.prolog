25f5f932ab7132de5573cc1d8e5ef3ce4c6bdffd
tonadev <alejandro.garciaes@alumno.buap.mx>
2022-09-05 11:16:50 -0500

    Añgrega el resto de comidas

--- IA_Equipo7.pl
+++ IA_Equipo7.pl
@@ -156,9 +156,29 @@ bebidas :-
 
 % :- inicio.
 
-comida('Carne de res', 15415, nutricion(6.36, 0, 27.7, 168), '2.2 anios', '4-12 meses congelado').
+% Carnes
+comida('Carne de res', 15415, nutricion(6.36, 0, 27.7, 168), '2.2 años', '4-12 meses congelado').
 comida('Carne de pollo', 4325, nutricion(3.24, 0, 32.1, 158), '49 dias', '12 meses congelado').
+comida('Carne de puerco', 5988, nutricion(9.04, 15.8, 7.38, 174), '23 - 25 semanas', '4-12 meses congelado').
+% Cereales
+comida('Arroz', 2497, nutricion(1.3, 79.8, 6.94, 369), '95-250 dias', '2 años sin abrir').
+comida('Pan', 1608, nutricion(1.65, 72.8, 14.3, 158), '49 dias', '12 meses congelado').
+comida('Pasta (seca)', 1849, nutricion(2.73, 72.1, 15.1, 370), '140 dias', '2 años sin abrir').
+% Verduras
+comida('Col', 237, nutricion(0.1, 5.8, 1.28, 25), '110-130 dias', '10-12 semanas congelado').
+comida('Papa', 287, nutricion(0.95, 79.9, 8.11, 361), '115 dias', '10-12 semanas congelado').
+comida('Jitomate', 214, nutricion(0.42, 3.84, 0.7, 22), '70-80 dias', '2 meses congelado').
+% Frutas
+comida('Manzana', 822, nutricion(0.15, 14.8, 0.13, 61), '131-148 dias', '8 meses congelado').
+comida('Platano', 790, nutricion(0.29, 23, 0.74, 98), '65-90 dias', '10-12 meses congelado').
+comida('Aceituna', 3025, nutricion(12.9, 4.96, 1.15, 141), '1 año', '2 semanas').
+% Bebidas
+comida('Vino', 109, nutricion(0, 1.15, 0.07, 49), '10-14 días de fermentación', '3-5 años sin abrir').
+comida('Leche', 255, nutricion(3.2, 4.63, 3.27, 61), '305 días de ordeño', '1-3 meses refrigerado sin abrir').
+comida('Cerveza', 74, nutricion(0, 3.55, 0.46, 43), '4-7 dias de fermentación', '4 meses').
 
+
+% Si Y se acerca a X con un margen de Z, entonces tiene caracteristicas similares.
 similar(X, Y, Z) :- (X < Y + Z,!); X > Y - Z.
 
 recomendacion(X, Y) :- comida(X,A1,nutricion(F1,CH1,P1,C1),_,_), comida(Y,A2,nutricion(F2,CH2,P2,C2),_,_), A2 < A1, 
